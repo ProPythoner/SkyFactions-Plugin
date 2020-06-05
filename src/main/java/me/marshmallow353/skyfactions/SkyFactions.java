bd32dbf7152a0355fa21f463fd9e39993369e5fa
ProPythoner <39158801+ProPythoner@users.noreply.github.com>
2020-06-05 23:20:36 +0100

    Removed imports
    Signed-off-by: ProPythoner <39158801+ProPythoner@users.noreply.github.com>

--- src/main/java/me/marshmallow353/skyfactions/SkyFactions.java
+++ src/main/java/me/marshmallow353/skyfactions/SkyFactions.java
@@ -5,7 +5,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.NotePlayEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
